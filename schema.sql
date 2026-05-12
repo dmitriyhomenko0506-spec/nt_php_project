5c4ba2b13a9a3a55ab1dc724d27ab3e6662a0268
Dmitriy Homenko <dmitriyhomenko0506@gmail.com>
2026-05-12 22:28:41 +0300

    mysql - 2 варианта

--- schema.sql
+++ schema.sql
@@ -1,3 +1,66 @@
+
+/* 
+courses — title, description → перелік курсів.
+lessons — course_id, title, video_url → уроки всередині курсу.
+users — name, email → акаунти слухачів або авторів.
+enrolments — user_id, course_id, progress_pct → запис користувача на курс і прогрес.
+tags — name → ключові слова.
+course_tag — course_id, tag_id → проміжна таблиця для many-to-many «курс ↔ тег».
+
+*/
+
+/* courses - Формируем список курсов */
+CREATE TABLE if not exists courses (
+    id SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL
+);
+
+
+/* lessons - Формируем список уроков */
+CREATE TABLE if not exists lessons (
+    id int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    course_id SMALLINT UNSIGNED NOT NULL,
+    video_url VARCHAR(255) NOT NULL,
+
+    FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
+);
+
+/* users - Создаем список пользователей */
+CREATE TABLE if not exists users (
+    id int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL
+);
+
+/* enrolments Запись пользователя на курс */
+CREATE TABLE if not exists enrolments (
+    id int UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    user_id int UNSIGNED NOT NULL,
+    course_id SMALLINT UNSIGNED NOT NULL,
+    progress_pct TINYINT UNSIGNED,
+
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE
+);
+
+/*tags  Таблица тэгов */
+CREATE TABLE if not exists tags (
+    id SMALLINT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(255) UNIQUE NOT NULL
+);
+
+/* course_tag  для many-to-many */
+CREATE TABLE if not exists course_tag (
+    course_id SMALLINT UNSIGNED NOT NULL,
+    tags_id SMALLINT UNSIGNED NOT NULL,
+    
+    PRIMARY KEY (course_id, tags_id), 
+    FOREIGN KEY (course_id) REFERENCES courses(id) ON DELETE CASCADE,
+    FOREIGN KEY (tags_id) REFERENCES tags(id) ON DELETE CASCADE
+);
+
+
 /*
 clinics — name, city → список клінік.
 vets — clinic_id, name, specialty → ветеринари певної клініки.
