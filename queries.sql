e02eb1c9ac9616dd6e5fdb95548efe2b487f3d19
Dmitriy Homenko <dmitriyhomenko0506@gmail.com>
2026-05-16 06:28:11 +0300

    add sql top - doctor

--- queries.sql
+++ queries.sql
@@ -33,9 +33,9 @@ app.scheduled_for as time,
 pet.name as pet_name,
 pet.species as type_pets
 FROM clinics as c
-JOIN vets as v ON v.clinic_id = c.id
-JOIN appointments as app ON app.vet_id = v.id
-JOIN pets as pet ON pet.id = app.pet_id;
+JOIN  vets as v ON v.clinic_id = c.id
+JOIN  appointments as app ON app.vet_id = v.id
+JOIN  pets as pet ON pet.id = app.pet_id;
 
 /* Записи в каких клиниках и у какого врача + что за животное + кто хозяин */
 SELECT 
@@ -48,10 +48,10 @@ pet.species as type_pets,
 o.name as name_owner,
 o.phone as phone_owner
 FROM clinics as c
-JOIN vets as v ON v.clinic_id = c.id
-JOIN appointments as app ON app.vet_id = v.id
-JOIN pets as pet ON pet.id = app.pet_id
-JOIN pet_owners as o ON o.id = pet.owner_id;
+JOIN  vets as v ON v.clinic_id = c.id
+JOIN  appointments as app ON app.vet_id = v.id
+JOIN  pets as pet ON pet.id = app.pet_id
+JOIN  pet_owners as o ON o.id = pet.owner_id;
 
 /* Чтобы получить по какой-то конкретной клинике добавить WHERE c.id = FK клиники (ID) */
 
@@ -67,7 +67,19 @@ o.name as name_owner,
 o.phone as phone_owner
 FROM clinics as c
 JOIN vets as v ON v.clinic_id = c.id
-JOIN appointments as app ON app.vet_id = v.id
-JOIN pets as pet ON pet.id = app.pet_id
-JOIN pet_owners as o ON o.id = pet.owner_id
+JOIN  appointments as app ON app.vet_id = v.id
+JOIN  pets as pet ON pet.id = app.pet_id
+JOIN  pet_owners as o ON o.id = pet.owner_id
 ORDER BY app.scheduled_for;
+
+/* Для сатистики: ТОП врач */
+SELECT 
+    v.name as doc_name,
+    v.specialty as doc_specialty,
+    COUNT(app.id) as doctor_appointments
+FROM vets as v
+JOIN appointments as app ON app.vet_id = v.id
+GROUP BY v.id, v.name, v.specialty
+HAVING COUNT(app.id) > (
+    SELECT COUNT(id) / COUNT(DISTINCT vet_id) FROM appointments
+);
